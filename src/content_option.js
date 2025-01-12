153d8a3667f6e324f726abc34b0c32dfd140ba22
mrkabby <antwikenneth14@gmail.com>
2025-01-12 01:30:04 +0000

    updated my Content Options with more projects

--- src/content_option.js
+++ src/content_option.js
@@ -110,12 +110,12 @@ const dataportfolio = [{
     },
     {
         img: "https://images.unsplash.com/photo-1464226184884-fa280b87c399?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YWdybyUyMG1hcmtldHxlbnwwfHwwfHx8MA%3D%3D",
-        description: "Created an ecommerce site.",
-        link: "https://660fade53aa29764451e1302--thriving-buttercream-0f4a05.netlify.app/",
+        description: "Created an  Agric ecommerce site.",
+        link: "https://https://farmyard-app.vercel.app/",
     },
     {
         img: "https://images.unsplash.com/photo-1483985988355-763728e1935b?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvcHBpbmd8ZW58MHx8MHx8fDA%3D",
-        description: "Created another ecommerce site.",
+        description: "Created Fashion ecommerce site.",
         link: "https://slay-shoper-app.vercel.app/",
     },
     {
@@ -123,7 +123,51 @@ const dataportfolio = [{
         description: "Created a PokeBook App.",
         link: "https://pokebook-coral.vercel.app/",
     },
-    
+    {
+        img: "https://hospital-app-peach.vercel.app/static/media/3_angels_suit_page-0001-removebg-preview.4001a4f9a65ac3e8c655.png",
+        description: "Created a Hospital App.",
+        link: "hospital-app-peach.vercel.app",
+    },
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-EvWNJ4gMLmBkIQENzJKdZkq1rMKnC6QbGw&s",
+        description: "Created a Travel App.",
+        link: "",
+    },
+    {
+        img: "https://premium-star-realty.vercel.app/assets/logo-D9NGESwe.png",
+        description: "Created a Real Estate App.",
+        link: "https://premium-star-realty.vercel.app/",
+    },
+    {
+        img: "https://rackets-for-africa-app.vercel.app/static/media/Logo.ce9f917a6a25c48120f1.png",
+        description: "Created an NGO  App.",
+        link: "https://rackets-for-africa-app.vercel.app/",
+    },
+    {
+        img: "https://ken16-foundation.vercel.app/static/media/ken16f.53b4713559073f8e37e6.jpg",
+        description: "Created another NGO site.",
+        link: "https://ken16-foundation.vercel.app/",
+    },
+    {
+        img: "https://duke-tennis-ukzn.vercel.app/static/media/DukeTennis.ba1fa4b67da996104162.jpg",
+        description: "Created Tennis site.",
+        link: "https://duke-tennis-ukzn.vercel.app/",
+    },
+    {
+        img: "https://i.imgur.com/IOXblrq.jpg",
+        description: "Created Delivery Mat site using GoogleSheet as CMS.",
+        link: "https://delivery-mat.vercel.app/",
+    },
+    {
+        img: "https://i.imgur.com/Ibi5MOS.jpg",
+        description: "Created Renaiz Farm site using GoogleSheet as CMS.",
+        link: "https://agyeiwaa-irrigation.vercel.app/",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1710149484964-d966b771c204?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZHJpZWQlMjByaWNlfGVufDB8fDB8fHww",
+        description: "Created GloryFoods site Using GoogleSheet as CMS.",
+        link: "https://glory-foods.vercel.app/",
+    },
 ];
 
 const contactConfig = {
